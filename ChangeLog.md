e238139341d56e62a648fea9f28eae39a76169cb
ashotbes <ashot.bes@gmail.com>
2020-02-15 00:14:51 +0400

    Start

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for irregularity
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Data.Text as T
+import Data.Text.IO as TIO 
+import CheckVerb
+
+main :: IO ()
+main = do 
+    Prelude.putStrLn "Please, enter the word, without spaces! "
+    someVerb <- TIO.getLine 
+    TIO.putStrLn $ (checkVerb someVerb)
+     
+
