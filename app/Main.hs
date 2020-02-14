94d4ab3515c7f5c0160442d50fea7948e51dbfb3
ashotbes <ashot.bes@gmail.com>
2020-02-15 00:16:01 +0400

    Startt

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Data.Text as T
 import Data.Text.IO as TIO 
 import CheckVerb
-
+    
 main :: IO ()
 main = do 
     Prelude.putStrLn "Please, enter the word, without spaces! "
